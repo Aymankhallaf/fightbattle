3de9c215f20df497c6ae791d9cf7fea0fed67efe
khallaf <engayman.khalaf@gmail.com>
2024-04-11 13:30:08 +0200

    strat comments

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ players = [
   },
   {
     name: "Samir",
-    life: 0,
+    life: 50,
     experience: 7,
     WeapanPower: 7,
     ShieldPower: 4,
@@ -34,9 +34,9 @@ players = [
   {
     name: "Sara",
     life: 50,
-    experience: 3,
-    WeapanPower: 3,
-    ShieldPower: 8,
+    experience: 1,
+    WeapanPower: 1,
+    ShieldPower: 1,
   },
   {
     name: "lili",
@@ -89,19 +89,6 @@ function getActivePlayers(players) {
   return players.filter((p) => p.life > 0);
 }
 
-// /**
-//  * get the Tournament Winner by unsing two functions isGameOver() and getActivePlayers().
-//  * @param {object} activePlayers an object of active players
-//  * @returns {object} object of the winner/winners.
-//  */
-// function getTournamentWinner(tournementResults) {
-//     let tournementResults=[];
-//     let tournementResults.winner={};
-//   return tournementResults[
-//     tournementResults.winner[tournementResults.length - 1]
-//   ];
-// }
-
 /**
  * get ranodm attacker and players (it can't be thE same personage).
  * @param {object} activePlayers a list of active players.
@@ -116,33 +103,34 @@ function getRandomAttackerAndDefender(activePlayers) {
   return [attacker, defender];
 }
 
-
-
-
-console.log(attackRandom(players[0]));
-
 function getFightResult(attackerAndDefender) {
-  let fightResult = {};
-  fightResult.attacker = attackerAndDefender[0];
-  fightResult.defender = attackerAndDefender[1];
-  fightResult.attacker.attackPoints = attackRandom(fightResult.attacker);
-
-  fightResult.defender.defendPoints = defendRandom(fightResult.defender);
-  if (fightResult.defender.attackPoints > fightResult.defender.defendPoints) {
-    fightResult.winner = fightResult.attacker;
-    fightResult.looser = fightResult.defender;
-    fightResult.defender.life -= fightResult.attacker.attackPoints;
+  let winner = "";
+  let looser = "";
+  let attacker = attackerAndDefender[0];
+  let defender = attackerAndDefender[1];
+  let attackPoints = attackRandom(attacker);
+  let defendPoints = defendRandom(defender);
+  if (attackPoints > defendPoints) {
+    winner = attacker.name;
+    looser = defender.name;
+    defender.life -= attackPoints;
   } else {
-    fightResult.winner = fightResult.defender;
-    fightResult.looser = fightResult.attacker;
+    winner = defender.name;
+    looser = attacker.name;
   }
- 
-  return fightResult;
+  return {
+    "Attacker name": attacker.name,
+    "Defender name": defender.name,
+    "winner name": winner,
+    "looser name": looser,
+    "attack Points": attackPoints,
+    "defend points": defendPoints,
+    "attaker life": attacker.life,
+    "defender life": defender.life,
+  };
 }
 
-
-
-
+console.table(players);
 tournementResults = [];
 let activePlayers = players;
 while (!isGameOver(activePlayers)) {
@@ -152,95 +140,5 @@ while (!isGameOver(activePlayers)) {
   activePlayers = getActivePlayers(players);
 }
 
-//raw data
 console.table(tournementResults);
 console.table(players);
-
-let showAllResult = [];
-for (let result of tournementResults) {
-  let showResult = {
-    "Attacker name": result.attacker.name,
-    "Defender name": result.defender.name,
-    "winner name": result.winner.name,
-    "looser name": result.looser.name,
-    "attack Points": result.attacker.attackPoints,
-    "defend points": result.defender.defendPoints,
-    "attaker life": result.attacker.life,
-    "defender life": result.defender.life,
-  };
-  
-
-  showAllResult.push(showResult);
-}
-
-console.table(showAllResult);
-
-// while (!isGameOver(players)) {
-//   getTournamentWinner(activePlayers);
-//   let attackerAndDefender = getRandomAttackerAndDefender(activePlayers);
-//   getWinnerAndLooser(attackerAndDefender);
-//   tournementResults.push(getfightRoundResult(attackerAndDefender));
-//   console.log(isGameOver);
-//   getActivePlayers(players);
-// }
-// console.table(tournementResults);
-
-// while (notFinished) {
-//   let activePlayers = players.filter((p) => p.life > 0);
-//   if (activePlayers.length == 1) {
-//     allResult.allTimeWinner = activePlayers;
-//     console.log(allResult.allFightWinner);
-//     notFinished = false;
-//     break;
-//   }
-//   results.attacker =
-//     activePlayers[Math.floor(Math.random() * activePlayers.length)];
-//   do {
-//     results.defender =
-//       activePlayers[Math.floor(Math.random() * activePlayers.length)];
-//   } while (results.defender === results.attacker);
-//   checkRoundWinner(results.attacker, results.defender);
-// }
-
-// console.table(players);
-// console.table(allResult);
-
-// results = [{players:{}, playersOut{}, attacker: "samir", defender: "lili", winner: "samir" }];
-
-// function attack(p) {
-//   return p.experience + Math.floor(Math.random() * (p.WeapanPower + 1));
-// }
-
-// function defend(p) {
-//   return p.experience + Math.floor(Math.random() * (p.ShieldPower + 1));
-// }
-
-// function attackResult(p1, p2) {
-//   return attack(p1) > defend(p2) ? p2.life - attack(p1) : 0;
-// }
-
-// notFinished = true;
-// let results = {};
-
-// function randomPlayerChoice(pLst) {}
-
-// notFinished = true;
-// let results = {};
-// let roundNumber = 0;
-// while (notFinished) {
-//   let activePlayers = players.filter((p) => p.life > 0);
-//   results[roundNumber][players] = activePlayers;
-//   let winner = {};
-//   results[winner] = winner;
-//   let attacker = {};
-//   if (activePlayers.length == 1) {
-//     winner = activePlayers;
-//     console.log(winner);
-//     notFinished = false;
-//     break;
-//   }
-
-//   console.log("ok");
-//   break;
-//   roundNumber++;
-// }
